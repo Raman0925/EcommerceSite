3b38e08c5f0ddffea4ba7eda31f2f8475d8907a7
Vaishnavi231860 <vishipal1208@gmail.com>
2025-10-02 22:38:43 +0530

    some changes in tailwind css

--- src/modules/policies/components/PrivacyPolicy.tsx
+++ src/modules/policies/components/PrivacyPolicy.tsx
@@ -1,14 +1,18 @@
 const Privacy = () => {
   return (
-    <div>
-      <h1>Privacy policy</h1>
-      <p>Last updated: April 23, 2025</p>
-      <br/>
-       <p>This Privacy Policy describes how House of Amuzo (the `Site`, `we`, `us`, or `our`) collects, uses, and discloses your personal information when you visit, use our services, or make a purchase from amuzo.in (the `Site`) or otherwise communicate with us regarding the Site (collectively, the `Services`). For purposes of this Privacy Policy, `you` and `your` means you as the user of the Services, whether you are a customer, website visitor, or another individual whose information we have collected pursuant to this Privacy Policy.
-      </p> 
-      <br/>
-      <p>Please read this Privacy Policy carefully. By using and accessing any of the Services, you agree to the collection, use, and disclosure of your information as described in this Privacy Policy. If you do not agree to this Privacy Policy, please do not use or access any of the Services.</p>
-      <br/>
+    <div className="flex flex-col items-center justify-center max-w-3xl mx-auto px-4 py-10">
+      <h1 className="text-5xl font-medium text-center text-gray-800 mb-4">Privacy policy</h1>
+      <br />
+      <p className="text-md text-gray-500">Last updated: April 23, 2025</p>
+      <br />
+      <p className="text-lg text-gray-600">This Privacy Policy describes how House of Amuzo (the `Site`, `we`, `us`, or `our`) collects, uses, and discloses your personal information when you visit, use our services, or make a purchase from amuzo.in (the `Site`) or otherwise communicate with us regarding the Site (collectively, the `Services`). For purposes of this Privacy Policy, `you` and `your` means you as the user of the Services, whether you are a customer, website visitor, or another individual whose information we have collected pursuant to this Privacy Policy.
+      </p>
+      <br />
+      <p className="text-lg text-gray-600">Please read this Privacy Policy carefully. By using and accessing any of the Services, you agree to the collection, use, and disclosure of your information as described in this Privacy Policy. If you do not agree to this Privacy Policy, please do not use or access any of the Services.</p>
+      <br />
+      <h2 className="text-4xl font-medium text-center text-gray-800 mb-4">Changes to This Privacy Policy</h2>
+      <br />
+      <p className="text-lg text-gray-600">We may update this Privacy Policy from time to time, including to reflect changes to our practices or for other operational, legal, or regulatory reasons. We will post the revised Privacy Policy on the Site, update the `Last updated` date and take any other steps required by applicable law.</p>
     </div>
   )
 };
