cfe798b96fafdddfe9a8429cc149734cab044f23
Vaishnavi231860 <vishipal1208@gmail.com>
2025-10-02 21:42:45 +0530

    change the file folders name in pascalcase

---
+++ src/modules/policies/components/PrivacyPolicy.tsx
@@ -0,0 +1,16 @@
+const Privacy = () => {
+  return (
+    <div>
+      <h1>Privacy policy</h1>
+      <p>Last updated: April 23, 2025</p>
+      <br/>
+       <p>This Privacy Policy describes how House of Amuzo (the `Site`, `we`, `us`, or `our`) collects, uses, and discloses your personal information when you visit, use our services, or make a purchase from amuzo.in (the `Site`) or otherwise communicate with us regarding the Site (collectively, the `Services`). For purposes of this Privacy Policy, `you` and `your` means you as the user of the Services, whether you are a customer, website visitor, or another individual whose information we have collected pursuant to this Privacy Policy.
+      </p> 
+      <br/>
+      <p>Please read this Privacy Policy carefully. By using and accessing any of the Services, you agree to the collection, use, and disclosure of your information as described in this Privacy Policy. If you do not agree to this Privacy Policy, please do not use or access any of the Services.</p>
+      <br/>
+    </div>
+  )
+};
+
+export default Privacy;
