7a0e265213e57fc250a5f4a4433a7a0630623230
Vaishnavi231860 <vishipal1208@gmail.com>
2025-10-02 18:45:44 +0530

    i have done the styling through tailwind in this contact file

--- src/modules/policies/components/Contact.tsx
+++ src/modules/policies/components/Contact.tsx
@@ -1,14 +1,13 @@
 import Image from 'next/image';
+import contact from '@/assets/contact.webp';
 
 const Contact = () => {
     return (
-        <div>
-            <div>
-                <h1>Contact Information</h1>
-                <p>Contact us via chat box, and we are super happy to help.</p>
-                <p>Or drop us an email on contact@amuzo.in. Or reach us on 9123355004</p>
-                {/* <Image src={contact} alt="Contact" /> */}
-            </div>
+        <div className="flex flex-col items-center justify-center p-9">
+            <h1 className="text-5xl font-medium text-center text-gray-800 mb-9">Contact Information</h1>
+            <p className='text-center text-gray-600 mb-6 text-xl'>Contact us via chat box, and we are super happy to help.</p>
+            <p className=' text-gray-600 mb-6 text-lg'>Or drop us an email on contact@amuzo.in. Or reach us on 9123355004</p>
+            <Image className='mx-auto rounded-xl max-w-2xl shadow-lg' src={contact} alt="family illustration" />
         </div>
     )
 }
