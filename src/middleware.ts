fe81a06019bfcf649b142bc715b984080166c56c
Vaishnavi231860 <vishipal1208@gmail.com>
2025-10-23 20:11:56 +0530

    added src > app > (auth) > sign-in > page.tsx in authSystem branch

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,19 @@
 import { NextResponse } from "next/server";
+// export { default } from "next-auth/middleware";
 import { getToken } from "next-auth/jwt";
+
 import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  return NextResponse.redirect(new URL("/home", req.url));
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  const url = req.nextUrl;
+  if (token) {
+    if (url.pathname.startsWith("/sign-in") || url.pathname.startsWith("/sign-up") || url.pathname.startsWith("/verify") || url.pathname.startsWith("/")) {
+      return NextResponse.redirect(new URL("/dashboard", req.url));
+    }
+  }
+  return NextResponse.next();
 }
 export const config = {
-  matcher: ['/sign-in', '/sign-up', '/', '/dashboard/:path*','/verify/:path*'],
+  matcher: ["/sign-in", "/sign-up", "/", "/dashboard/:path*", "/verify/:path*"],
 };
