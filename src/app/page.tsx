999135398473b2a1087467c234a80932346cd714
Raman0925 <ramankumarkannaujiya@gmail.com>
2025-12-06 18:48:46 +0530

    feat: build homepage with featured and latest products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
+import ProductList from "@/components/shared/product/product-list";
+import {
+  getFeaturedProducts,
+  getLatestProducts,
+} from "@/lib/actions/product.actions";
+import ProductCarousel from "@/components/shared/product/product-carousel";
+import ViewAllProductsButton from "@/components/view-all-products-button";
 
-export default function Home() {
+export default async function Home() {
+  const latestProducts = await getLatestProducts();
+  const featuredProducts = await getFeaturedProducts();
+
   return (
-    <div>
-      <h1>hello world</h1>
+    <div className="space-y-8">
+      {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+      )}
+      <ProductList title="Newest Arrivals" data={latestProducts} />
+      <ViewAllProductsButton />
     </div>
   );
 }
-
