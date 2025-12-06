4b547292a347123e52b2c720b3bd646951ed0ecc
Raman0925 <ramankumarkannaujiya@gmail.com>
2025-12-06 17:36:33 +0530

    Add user profile update action

--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -6,11 +6,13 @@ import { auth, signIn, signOut } from "@/auth";
 import { prisma } from "@/db/prisma";
 import { formatError } from "@/lib/utils";
 import {
+  paymentMethodSchema,
   shippingAddressSchema,
   signInFormSchema,
   signUpFormSchema,
 } from "@/lib/validator";
 import { ShippingAddress } from "@/types";
+import { z } from "zod";
 
 // Sign in the user with credentials
 export async function signInWithCredentials(
@@ -122,6 +124,66 @@ export async function updateUserAddress(data: ShippingAddress) {
     await prisma.user.update({
       where: { id: currentUser.id },
       data: { address },
+    });
+
+    return {
+      success: true,
+      message: "User updated successfully",
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
+
+export async function updateProfile(user: { name: string; email: string }) {
+  try {
+    const session = await auth();
+
+    const currentUser = await prisma.user.findFirst({
+      where: {
+        id: session?.user.id,
+      },
+    });
+
+    if (!currentUser) {
+      throw new Error("User not found");
+    }
+
+    await prisma.user.update({
+      where: {
+        id: currentUser.id,
+      },
+      data: {
+        name: user.name,
+      },
+    });
+
+    return {
+      success: true,
+      message: "User updated successfully",
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
+
+export async function updateUserPaymentMethod(
+  data: z.infer<typeof paymentMethodSchema>
+) {
+  try {
+    const session = await auth();
+    const currentUser = await prisma.user.findFirst({
+      where: { id: session?.user.id! },
+    });
+    if (!currentUser) {
+      throw new Error("User not found");
+    }
+
+    const paymentMethod = paymentMethodSchema.parse(data);
+
+    await prisma.user.update({
+      where: { id: currentUser.id },
+      data: { paymentMethod: paymentMethod.type },
     });
 
     return {
